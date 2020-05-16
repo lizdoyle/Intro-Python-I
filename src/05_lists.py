dfc34a54a3a19daba96d200ac88c29227bf304b0
lizdoyle <lizdoyle1121@gmail.com>
2020-05-15 21:26:05 -0400

    lists

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,11 +8,15 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
-print(x)
+
+print(x + y)
+
+#NOT CORRECT BUT WORKS
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
